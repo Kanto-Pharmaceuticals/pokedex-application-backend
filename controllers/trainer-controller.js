c71bc6722bc246ef8862f5abeb7e9189a460058b
Matthew Poon <mpoon2@athabasca.edu>
2022-09-20 19:40:09 -0400

    typo fixes

--- controllers/trainer-controller.js
+++ controllers/trainer-controller.js
@@ -1,7 +1,7 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const asyncHandler = require("express-async-handler");
-const Trainer = require("../models/trainer-model");
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const asyncHandler = require("express-async-handler")
+const Trainer = require("../models/trainer-model")
 
 /**
  * Register a new trainer
@@ -9,26 +9,26 @@ const Trainer = require("../models/trainer-model");
  * @access Public
  */
 const registerTrainer = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password } = req.body
 
   // check if necessary fields were entered
   if (!name || !email || !password) {
-    res.status(400);
-    throw new Error("Please fill in all fields");
+    res.status(400)
+    throw new Error("Please fill in all fields")
   }
 
   // check if a trainer exists with the same
-  const trainerExists = await Trainer.findOne({ name });
+  const trainerExists = await Trainer.findOne({ name })
 
   // throw a new error if a trainer already exists with the same email or name
   if (trainerExists) {
-    res.status(400);
-    throw new Error("Trainer already exists");
+    res.status(400)
+    throw new Error("Trainer already exists")
   }
 
   // generate salt and hash the password using it
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
 
   // create a new trainer using the provided fields with the default role
   const trainer = await Trainer.create({
@@ -36,7 +36,7 @@ const registerTrainer = asyncHandler(async (req, res) => {
     email,
     password: hashedPassword,
     roles: "Novice",
-  });
+  })
 
   // return status depending on outcome
   if (trainer) {
@@ -47,13 +47,13 @@ const registerTrainer = asyncHandler(async (req, res) => {
       email: trainer.email,
       roles: trainer.roles,
       token: generateToken(trainer._id),
-    });
+    })
   } else {
     // trainer data was invalid, throw a new error
-    res.status(400);
-    throw new Error("Invalid trainer data");
+    res.status(400)
+    throw new Error("Invalid trainer data")
   }
-});
+})
 
 /**
  * Authenticate a trainer
@@ -61,10 +61,10 @@ const registerTrainer = asyncHandler(async (req, res) => {
  * @access Public
  */
 const loginTrainer = asyncHandler(async (req, res) => {
-  const { name, password } = req.body;
+  const { name, password } = req.body
 
   // checks for the trainers name
-  const trainer = await Trainer.findOne({ name });
+  const trainer = await Trainer.findOne({ name })
 
   // compare the password with the one stored in database
   if (trainer && (await bcrypt.compare(password, trainer.password))) {
@@ -75,13 +75,13 @@ const loginTrainer = asyncHandler(async (req, res) => {
       email: trainer.email,
       roles: trainer.roles,
       token: generateToken(trainer._id),
-    });
+    })
   } else {
     // invalid credentials
-    res.status(400);
-    throw new Error("Invalid trainer credentials");
+    res.status(400)
+    throw new Error("Invalid trainer credentials")
   }
-});
+})
 
 /**
  * Get user data
@@ -89,18 +89,18 @@ const loginTrainer = asyncHandler(async (req, res) => {
  * @access Private
  */
 const getMe = asyncHandler(async (req, res) => {
-  res.status(200).json(req.trainer);
-});
+  res.status(200).json(req.trainer)
+})
 
 /**
  * Generates a JWT
  */
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
-};
+const generateToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" })
+}
 
 module.exports = {
   registerTrainer,
   loginTrainer,
   getMe,
-};
+}
